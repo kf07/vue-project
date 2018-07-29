f901d21a9d8bd0f6e96a09f40d88946d77c2aa37
furusu <furusu0715@gmail.com>
2018-07-30 00:04:45 +0900

    メモをfirebaseのデータベースに保存、リロードで読み込み

--- mymarkdown/src/components/Editor.vue
+++ mymarkdown/src/components/Editor.vue
@@ -10,6 +10,7 @@
                 </div>
                 <button class="addMemoBtn" @click="addMemo">メモの追加</button>
                 <button class="deleteBtn" v-if="memos.length > 1" @click="deleteMemo">選択中のメモの削除</button>
+                <button class="saveMemosBtn" @click="saveMemos">メモの保存</button>
             </div>
             <textarea class="markdown" v-model="memos[selectedIndex].markdown"></textarea>
             <div class="preview" v-html="preview()"></div>
@@ -33,6 +34,17 @@
                 selectedIndex: 0
             };
         },
+        created: function(){
+            firebase
+                .database()
+                .ref('memos/' + this.user.uid)
+                .once('value')
+                .then(result => {
+                    if(result.val()){
+                        this.memos = result.val();
+                    }
+                })
+        },
         methods: {
             logout: function () {
                 firebase.auth().signOut();
@@ -41,6 +53,12 @@
                 this.memos.push({
                     markdown: "無題のメモ"
                 });
+            },
+            saveMemos:function(){
+              firebase
+                  .database()
+                  .ref('memos/' + this.user.uid)
+                  .set(this.memos);
             },
             deleteMemo: function(){
                 this.memos.splice(this.selectedIndex,1);
