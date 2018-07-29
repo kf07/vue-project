487d971fb867fbc370d3be94760b884ed97e9716
furusu <furusu0715@gmail.com>
2018-07-29 23:54:19 +0900

    選択中のメモの削除機能

--- mymarkdown/src/components/Editor.vue
+++ mymarkdown/src/components/Editor.vue
@@ -9,6 +9,7 @@
                     <p class="memoTitle">{{ displayTitle(memo.markdown) }}</p>
                 </div>
                 <button class="addMemoBtn" @click="addMemo">メモの追加</button>
+                <button class="deleteBtn" v-if="memos.length > 1" @click="deleteMemo">選択中のメモの削除</button>
             </div>
             <textarea class="markdown" v-model="memos[selectedIndex].markdown"></textarea>
             <div class="preview" v-html="preview()"></div>
@@ -40,6 +41,12 @@
                 this.memos.push({
                     markdown: "無題のメモ"
                 });
+            },
+            deleteMemo: function(){
+                this.memos.splice(this.selectedIndex,1);
+                if(this.selectedIndex > 0) {
+                    this.selectedIndex--;
+                }
             },
             selectMemo: function(index){
                 this.selectedIndex = index;
